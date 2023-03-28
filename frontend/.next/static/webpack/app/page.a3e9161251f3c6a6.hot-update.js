01af1a01f48e0f3c7301df0e579f482f4a032d75
AaronTan <tanweihup@gmail.com>
2023-03-28 14:52:11 +0800

    Initial Empty Project Commit

---
+++ frontend/.next/static/webpack/app/page.a3e9161251f3c6a6.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Faaron%2FDeveloper%2FSDP-CarePet%2Ffrontend%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2FUsers%2Faaron%2FDeveloper%2FSDP-CarePet%2Ffrontend%2Fapp%2Fpage.module.css&server=false!":
+/*!************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Faaron%2FDeveloper%2FSDP-CarePet%2Ffrontend%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2FUsers%2Faaron%2FDeveloper%2FSDP-CarePet%2Ffrontend%2Fapp%2Fpage.module.css&server=false! ***!
+  \************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/image.js */ \"(app-client)/./node_modules/next/dist/client/image.js\", 23));\n(() => __webpack_require__.e(/*! import() */ \"_app-client_app_page_module_css\").then(__webpack_require__.t.bind(__webpack_require__, /*! ./app/page.module.css */ \"(app-client)/./app/page.module.css\", 23)))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRlVzZXJzJTJGYWFyb24lMkZEZXZlbG9wZXIlMkZTRFAtQ2FyZVBldCUyRmZyb250ZW5kJTJGbm9kZV9tb2R1bGVzJTJGbmV4dCUyRmRpc3QlMkZjbGllbnQlMkZpbWFnZS5qcyZtb2R1bGVzPSUyRlVzZXJzJTJGYWFyb24lMkZEZXZlbG9wZXIlMkZTRFAtQ2FyZVBldCUyRmZyb250ZW5kJTJGYXBwJTJGcGFnZS5tb2R1bGUuY3NzJnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEseU1BQXVIO0FBQ3ZILE9BQU8scU1BQW1HIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/YmI3ZSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIi9Vc2Vycy9hYXJvbi9EZXZlbG9wZXIvU0RQLUNhcmVQZXQvZnJvbnRlbmQvbm9kZV9tb2R1bGVzL25leHQvZGlzdC9jbGllbnQvaW1hZ2UuanNcIik7XG4oKCkgPT4gaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImxhenlcIiAqLyBcIi9Vc2Vycy9hYXJvbi9EZXZlbG9wZXIvU0RQLUNhcmVQZXQvZnJvbnRlbmQvYXBwL3BhZ2UubW9kdWxlLmNzc1wiKSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Faaron%2FDeveloper%2FSDP-CarePet%2Ffrontend%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2FUsers%2Faaron%2FDeveloper%2FSDP-CarePet%2Ffrontend%2Fapp%2Fpage.module.css&server=false!\n"));
+
+/***/ })
+
+});
