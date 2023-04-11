8bbbee922ae2b8662f104b6a5f4acbf667c368f4
AaronTan <tanweihup@gmail.com>
2023-04-12 00:23:36 +0800

    changed styling for header
    added flex to header and made the navigation list looks nicer with the sign in/sign up button have a better look but without the linear colour yet

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -5,21 +5,39 @@ import HeaderStyles from "../../styles/components/Header.module.scss";
 export default function Header() {
    return (
       <>
-         <div>
-            <Link href={"/"}>
-               <Image
-                  src="/Logo.png"
-                  alt="A Paw Logo of CarePet"
-                  width="30"
-                  height="30"
-               />
-               <span className={HeaderStyles.logo}>
-                  Care<span className={HeaderStyles.logoTitle}>Pet</span>
-               </span>
-            </Link>
-         </div>
-
-         <div>Sign In</div>
+         <nav className={HeaderStyles.heroNav}>
+            <div>
+               <Link href={"/"}>
+                  <Image
+                     src="/Logo.png"
+                     alt="A Paw Logo of CarePet"
+                     width="30"
+                     height="30"
+                  />
+                  <span className={HeaderStyles.logo}>
+                     Care<span className={HeaderStyles.logoTitle}>Pet</span>
+                  </span>
+               </Link>
+            </div>
+            <div>
+               <Link href={"/"} className={HeaderStyles.navItems}>
+                  Home
+               </Link>
+               <Link href={"/"} className={HeaderStyles.navItems}>
+                  About
+               </Link>
+               <Link href={"/"} className={HeaderStyles.navItems}>
+                  Blog
+               </Link>
+               <Link href={"/"} className={HeaderStyles.navItems}>
+                  Services
+               </Link>
+            </div>
+            <div>
+               <span className={HeaderStyles.navButtons}>Sign In</span>
+               <button className={HeaderStyles.navButtons}>Sign Up</button>
+            </div>
+         </nav>
       </>
    );
 }
