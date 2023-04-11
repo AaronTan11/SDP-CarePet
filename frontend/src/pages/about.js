1eed17af6495e4a49f46a11c191bbb26313ee301
AaronTan <tanweihup@gmail.com>
2023-04-11 22:29:16 +0800

    added h3 tag

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -4,6 +4,7 @@ export default function About() {
          <main>
             <h1>This is about page</h1>
             <h2>h2 tag added</h2>
+            <h3>added h3 tag</h3>
          </main>
       </>
    );
