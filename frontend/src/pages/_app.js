f644492caf646734979302d0ab0d0fedb2dedc57
AaronTan <tanweihup@gmail.com>
2023-04-11 19:22:06 +0800

    added logo picture and title
    Also made the Logo as a link to homepage

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,5 @@
-import Layout from "../components/layout";
-import "@/styles/main.scss";
+import Layout from "../components/Layout";
+import styles from "../styles/main.scss";
 
 export default function App({ Component, pageProps }) {
    return (
