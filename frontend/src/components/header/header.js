3b526930be3fe463690445caa075e66dfceba31e
AaronTan <tanweihup@gmail.com>
2023-04-12 14:27:13 +0800

    header is completed

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
-import HeaderStyles from "../../styles/components/Header.module.scss";
+import styles from "../../styles/components/Header.module.scss";
 
 export default function Header() {
    return (
       <>
-         <nav className={HeaderStyles.heroNav}>
+         <nav className={`${styles.heroNav}`}>
             <div>
                <Link href={"/"}>
                   <Image
@@ -14,28 +14,30 @@ export default function Header() {
                      width="30"
                      height="30"
                   />
-                  <span className={HeaderStyles.logo}>
-                     Care<span className={HeaderStyles.logoTitle}>Pet</span>
+                  <span className={`${styles.logo}`}>
+                     Care<span className={`${styles.logoTitle}`}>Pet</span>
                   </span>
                </Link>
             </div>
             <div>
-               <Link href={"/"} className={HeaderStyles.navItems}>
+               <Link href={"/"} className={`${styles.navItems}`}>
                   Home
                </Link>
-               <Link href={"/"} className={HeaderStyles.navItems}>
+               <Link href={"/about"} className={`${styles.navItems}`}>
                   About
                </Link>
-               <Link href={"/"} className={HeaderStyles.navItems}>
+               <Link href={"/"} className={`${styles.navItems}`}>
                   Blog
                </Link>
-               <Link href={"/"} className={HeaderStyles.navItems}>
+               <Link href={"/"} className={`${styles.navItems}`}>
                   Services
                </Link>
             </div>
             <div>
-               <span className={HeaderStyles.navButtons}>Sign In</span>
-               <button className={HeaderStyles.navButtons}>Sign Up</button>
+               <span className={`${styles.navButtons} ${styles.logoTitle}`}>
+                  Sign In
+               </span>
+               <span className={`${styles.navButtons}`}>Sign Up</span>
             </div>
          </nav>
       </>
