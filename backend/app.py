f72ea8ec729b31abd00debf6c22c99c3d95f4ce0
AaronTan <tanweihup@gmail.com>
2023-05-01 19:36:06 +0800

    finished setting up backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,35 @@
+from database import db_session
+from flask import request, jsonify
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import models
+
+import dotenv
+import os
 
 app = Flask(__name__)
+CORS(app)
+
+dotenv.load_dotenv()
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!!!!</p>"
+def home():
+    return "Hello, World!"
 
 
+# Shutting down sessions
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+# Initialization
 if __name__ == "__main__":
+    from database import init_db
+
+    init_db()
     app.run(debug=True)
