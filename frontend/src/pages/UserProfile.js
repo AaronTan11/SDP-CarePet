9191c677352a2c26302755b8771a74780920cb86
AaronTan <tanweihup@gmail.com>
2023-05-05 14:48:18 +0800

    changed few pages links

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -2,19 +2,27 @@ import styles from "../styles/UserProfile.module.scss";
 import User from "../components/User";
 
 function UserProfile() {
-  return (
-    <div className={styles.container}>
-      <User />
-      <div className={styles.myaccount}>
-        <h1>Welcome, </h1>
-        <div className={styles.myaccountcontent}>
-          <b>Fullname:</b>
-          <b>Email:</b>
-          <b>Contact Number:</b>
-        </div>
+   const storedUser = localStorage.getItem("user");
+   const user = storedUser ? JSON.parse(storedUser) : null;
+
+   if (!user) {
+      // Redirect to the login page or show an error message
+      return <div>User not logged in</div>;
+   }
+
+   return (
+      <div className={styles.container}>
+         <User />
+         <div className={styles.myaccount}>
+            <h1>Welcome, {user.username}</h1>
+            <div className={styles.myaccountcontent}>
+               <b>Fullname: {user.fullname}</b>
+               <b>Email: {user.email}</b>
+               <b>Contact Number: {user.contactNumber}</b>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default UserProfile;
--- frontend/src/pages/UserSideNav.js
+++ frontend/src/pages/UserSideNav.js
@@ -1,20 +1,21 @@
+import Link from "next/link";
 import styles from "../styles/AdminSideNav.module.scss";
 
 function UserSideNav() {
-  return (
-    <div className={styles.sidenav}>
-      <img
-        className={styles.userImage}
-        src="images/user.png"
-        alt="image user"
-      ></img>
-      <div className={styles.sidenavContent}>
-        <a href="#">My Profile</a>
-        <a href="#">Check Records</a>
-        <a href="#">Log Out</a>
+   return (
+      <div className={styles.sidenav}>
+         <img
+            className={styles.userImage}
+            src="images/user.png"
+            alt="image user"
+         ></img>
+         <div className={styles.sidenavContent}>
+            <Link href="#">My Profile</Link>
+            <Link href="#">Check Records</Link>
+            <Link href="/">Log Out</Link>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default UserSideNav;
