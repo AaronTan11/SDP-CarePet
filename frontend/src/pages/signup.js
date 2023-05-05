f18dba4398e6516a780a7eb04f389ed85a4f51ae
Bryanlew086 <bryanlew2003@gmail.com>
2023-05-05 18:06:10 +0800

    added tables for database

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -2,93 +2,95 @@ import { useState } from "react";
 import styles from "../styles/Login.module.scss";
 import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
 function login() {
-   const [username, setUsername] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const registerUser = async (userData) => {
-      const response = await axios.post(
-         "http://localhost:5000/api/register",
-         userData
-      );
-      return response.data;
-   };
+  const router = useRouter();
 
-   const mutation = useMutation(registerUser, {
-      onSuccess: (data) => {
-         console.log(data); // Handle successful registration, e.g., show a success message or redirect to another page
-      },
-      onError: (error) => {
-         console.error("Error during registration:", error); // Handle error response, e.g., show an error message
-      },
-   });
+  const registerUser = async (userData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/register",
+      userData
+    );
+    return response.data;
+  };
 
-   const handleClick = (event) => {
-      event.preventDefault();
-      mutation.mutate({ username, email, password });
-   };
+  const mutation = useMutation(registerUser, {
+    onSuccess: (data) => {
+      console.log(data); // Handle successful registration, e.g., show a success message or redirect to another page
+      router.push("/login");
+    },
+    onError: (error) => {
+      console.error("Error during registration:", error); // Handle error response, e.g., show an error message
+    },
+  });
 
-   return (
-      <div className={styles.cover}>
-         <div className={styles.inside}>
-            <h1>Sign Up</h1>
-            <div className={styles.Bar}>
-               <form>
-                  <label>
-                     Username :
-                     <input
-                        className={styles.input}
-                        name="username"
-                        type="text"
-                        value={username}
-                        placeholder="Please enter username"
-                        onChange={(event) => setUsername(event.target.value)}
-                     ></input>
-                  </label>
-                  <br />
-                  <label>
-                     Email :
-                     <input
-                        className={styles.input}
-                        name="email"
-                        type="email"
-                        value={email}
-                        placeholder="Please enter email"
-                        onChange={(event) => setEmail(event.target.value)}
-                     ></input>
-                  </label>
-                  <br />
-                  <label>
-                     Password :
-                     <input
-                        className={styles.input}
-                        name="password"
-                        type="password"
-                        value={password}
-                        placeholder="Please enter password"
-                        onChange={(event) => setPassword(event.target.value)}
-                     ></input>
-                  </label>
-                  <br />
-               </form>
-            </div>
-            <button
-               className={styles.btn}
-               onClick={handleClick}
-               type="submit"
-               disabled={mutation.isLoading}
-            >
-               Sign Up
-            </button>
-            {mutation.isError && (
-               <p>Error: {mutation.error.response.data.error}</p>
-            )}
-            {mutation.isSuccess && <p>User registered successfully!</p>}
-         </div>
+  const handleClick = (event) => {
+    event.preventDefault();
+    mutation.mutate({ username, email, password });
+  };
+
+  return (
+    <div className={styles.cover}>
+      <div className={styles.inside}>
+        <h1>Sign Up</h1>
+        <div className={styles.Bar}>
+          <form>
+            <label>
+              Username :
+              <input
+                className={styles.input}
+                name="username"
+                type="text"
+                value={username}
+                placeholder="Please enter username"
+                onChange={(event) => setUsername(event.target.value)}
+              ></input>
+            </label>
+            <br />
+            <label>
+              Email :
+              <input
+                className={styles.input}
+                name="email"
+                type="email"
+                value={email}
+                placeholder="Please enter email"
+                onChange={(event) => setEmail(event.target.value)}
+              ></input>
+            </label>
+            <br />
+            <label>
+              Password :
+              <input
+                className={styles.input}
+                name="password"
+                type="password"
+                value={password}
+                placeholder="Please enter password"
+                onChange={(event) => setPassword(event.target.value)}
+              ></input>
+            </label>
+            <br />
+          </form>
+        </div>
+        <button
+          className={styles.btn}
+          onClick={handleClick}
+          type="submit"
+          disabled={mutation.isLoading}
+        >
+          Sign Up
+        </button>
+        {mutation.isError && <p>Error: {mutation.error.response.data.error}</p>}
+        {mutation.isSuccess && <p>User registered successfully!</p>}
       </div>
-   );
+    </div>
+  );
 }
 
 export default login;
