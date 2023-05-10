c1b85150fc579f4c2d8098d9c66af4764fe2483a
AaronTan <tanweihup@gmail.com>
2023-05-10 18:08:10 +0800

    commiting backend stuff

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from database import db_session
 from flask import request, jsonify
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from models import *
 from auth import *
+from profileGenerator import *
+from flask import make_response
+
 
 import dotenv
 import os
@@ -43,17 +45,34 @@ def register():
     hashed_password = bcrypt.generate_password_hash(
         data["password"]).decode("utf-8")
 
-    user = User(username=data["username"],
-                email=data["email"], password=hashed_password)
+    profile_pic = get_gravatar_url(data["email"])
+
+    if User.query.count() == 0:
+        # First user to register will be an admin
+        user = User(
+            username=data["username"],
+            email=data["email"],
+            password=hashed_password,
+            profile_pic=profile_pic,
+            user_role="admin"
+        )
+    else:
+        # Set default role to "user"
+        user = User(
+            username=data["username"],
+            email=data["email"],
+            password=hashed_password,
+            profile_pic=profile_pic,
+            user_role="user"
+        )
+
     db_session.add(user)
     db_session.commit()
 
-    token = generate_token(user.id)
-    return jsonify({"success": "User registered successfully", "token": token}), 201
+    return jsonify({"success": "User registered successfully"}), 201
+
 
 # Login route
-
-
 @app.route("/api/login", methods=["POST"])
 def login():
     data = request.get_json()
@@ -65,30 +84,52 @@ def login():
     if not user or not bcrypt.check_password_hash(user.password, data["password"]):
         return jsonify({"error": "Invalid email or password"}), 401
 
-    token = generate_token(user.id)
-    return jsonify({"success": "User logged in successfully", "user_id": user.id, "username": user.username, "token": token}), 200
+    return jsonify(
+        {"success": "User logged in successfully", "user_id": user.id, "username": user.username}), 200
+
+
+# Logout route
+@app.route('/api/logout', methods=['POST'])
+def logout():
+    return jsonify({"success": "User logged out successfully"}), 200
+
+
+# Get User Data
+@app.route("/api/user/<int:user_id>", methods=["GET"])
+def get_user_data(user_id):
+
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    bookings = [{"id": booking.id, "user_id": booking.user_id, "contact": booking.contact, "date": booking.date,
+                 "service_type": booking.service_type, "pet_breed": booking.pet_breed} for booking in user.bookings]
+
+    return jsonify({
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "profile_pic": user.profile_pic,
+        "user_role": user.user_role,
+        "contact": user.contact,
+        "bookings": bookings
+    }), 200
+
 
 # Booking
 @app.route("/api/Booking", methods=["POST"])
 def booking():
     data = request.get_json()
 
-    if not data or not data.get("name") or not data.get("contact") or not data.get("email") or not data.get("date"):
+    if not data or not data.get("name") or not data.get("contact") or not data.get("email") or not data.get("date") or not data.get("service_type") or not data.get("pet_breed"):
         return jsonify({"error": "Missing required fields"}), 400
-    
-    
-    booking = Booking(name=data['name'],contact=data['contact'],email=data['email'],Date=data['date'])
+
+    booking = Booking(user_id=data["user_id"], name=data["name"], contact=data["contact"],
+                      email=data["email"], date=data["date"], service_type=data["service_type"], pet_breed=data["pet_breed"])
     db_session.add(booking)
     db_session.commit()
 
-    return jsonify({"Success": "Book Successfully"}),201
-
-
-
-
-
-
-# Getting Users
+    return jsonify({"Success": "Book Successfully"}), 201
 
 
 # Shutting down sessions
--- backend/auth.py
+++ backend/auth.py
@@ -1,15 +1,39 @@
-import jwt
-from functools import wraps
-from flask import request, jsonify, make_response
-from datetime import datetime, timedelta
-import os
+# import jwt
+# from functools import wraps
+# from flask import request, jsonify, make_response
+# from datetime import datetime, timedelta
+# import os
+# from models import *
 
 
-def generate_token(user_id):
-    payload = {
-        'exp': datetime.utcnow() + timedelta(days=1),
-        'iat': datetime.utcnow(),
-        'sub': user_id,
-    }
-    token = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm='HS256')
-    return token
+# def token_required(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         token = None
+
+#         if 'Authorization' in request.headers:
+#             token = request.headers['Authorization']
+
+#         if not token:
+#             return jsonify({'error': 'Token is missing'}), 401
+
+#         try:
+#             data = jwt.decode(token, os.getenv(
+#                 'SECRET_KEY'), algorithms=["HS256"])
+#             current_user = User.query.filter_by(id=data['sub']).first()
+#         except:
+#             return jsonify({'error': 'Token is invalid'}), 401
+
+#         return f(current_user, *args, **kwargs)
+
+#     return decorated
+
+
+# def generate_token(user_id):
+#     payload = {
+#         'exp': datetime.utcnow() + timedelta(days=1),
+#         'iat': datetime.utcnow(),
+#         'sub': user_id,
+#     }
+#     token = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm='HS256')
+#     return token
